93188a3b8faef3bd01203a2b0db09813f27ef5fe
RigobertoPO <rperez@cobach.edu.mx>
2023-03-10 21:40:31 -0600

    LINEAS CONTINUAS

--- Demo3OpenGL8M/inicio.cpp
+++ Demo3OpenGL8M/inicio.cpp
@@ -1,13 +1,39 @@
 #include<gl/glut.h>
 void DibujarLineas() {
 	glClear(GL_COLOR_BUFFER_BIT);
+	glBegin(GL_LINES);
+	glVertex2i(100, 450);
+	glVertex2i(400, 450);
+	glEnd();
+	glBegin(GL_LINE_STRIP);
+	glVertex2i(100, 200);
+	glVertex2i(400, 200);
+	glVertex2i(400, 450);
+	glVertex2i(250, 550);
+	glVertex2i(100, 450);
+	glVertex2i(100, 200);
+	glEnd();
+	glBegin(GL_LINE_STRIP);
+	glVertex2i(250, 300);
+	glVertex2i(350, 300);
+	glVertex2i(350, 400);
+	glVertex2i(250, 400);
+	glVertex2i(250, 300);
+	glEnd();
+	glColor3f(0.0, 1.0, 1.0);
+	glBegin(GL_LINE_LOOP);
+	glVertex2i(450, 200);
+	glVertex2i(750, 200);
+	glVertex2i(750, 450);
+	glVertex2i(450, 450);
+	glEnd();
 	glFlush();
 }
 void IniciarConfiguracion() {
 	glClearColor(1, 0, 1, 1);
 	glColor3f(0.0, 0.0, 1.0);
-	glLineWidth(4.0);
-	glOrtho(800, 0, 600, 0, 1, -1);
+	glLineWidth(3.0);
+	glOrtho(0, 800, 0, 600, 1, -1);
 }
 int main(int argc, char* args[]) {
 	glutInit(&argc, args);
